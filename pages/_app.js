eba2d7c81bc0a8748bf0c71e0df6fa7a5a090683
Jim Ambras <jim.ambras@gmail.com>
2025-12-06 12:02:51 -0800

    Add Contentful logo to header on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '../styles/globals.css'
+import Image from 'next/image'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="min-h-screen">
+      <header className="fixed top-0 left-0 z-50 p-4">
+        <Image 
+          src="https://images.ctfassets.net/jtqsy5pye0zd/6wNuQ2xMvbw134rccObi0q/bf61badc6d6d9780609e541713f0bba6/Contentful_Logo_2.5_Dark.svg" 
+          alt="Contentful" 
+          width={160} 
+          height={34}
+          priority
+        />
+      </header>
+      <main className="pt-16">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
 }
